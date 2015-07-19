e00c61f3a7adc210cb94e657de14aceda1af31b7
alexbooker <alexbooker@fastmail.im>
2015-07-19 14:55:19 +0100

    initial commit

---
+++ cli.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var meow = require('meow');
+var unfollowEveryone = require('./');
+
+var cli = meow();
+
+var username = cli.input[0];
+var password = cli.input[1];
+
+unfollowEveryone(username, password, function(error) {
+  if (error) {
+    console.log('oops. an error occured:', error);
+    return;
+  }
+  console.log('done.');
+});
---
+++ index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var GitHub = require('github');
+var async = require('async');
+
+var github = new GitHub({
+  version: '3.0.0'
+});
+
+function fetchAllFollowings(done) {
+  (function fetch(page, result) {
+    var PER_PAGE = 100;
+    github.user.getFollowing({
+      page: page,
+      per_page: PER_PAGE
+    }, function(error, users) {
+      if (error) {
+        done(error);
+        return;
+      }
+      // aggregate followings
+      result = result.concat(users);
+      var next = users.length === PER_PAGE;
+      if (next) {
+        fetch(page + 1);
+        return;
+      }
+      done(null, result);
+    });
+  })(1, []);
+}
+
+function unfollowEveryone(users, done) {
+  async.each(
+    users,
+    function(user, done) {
+      github.user.unFollowUser({
+        user: user.login
+      }, function(error, response) {
+        done(error, response);
+      });
+    },
+    done
+  );
+}
+
+module.exports = function(username, password, done) {
+  github.authenticate({
+    type: 'basic',
+    username: username,
+    password: password
+  });
+  fetchAllFollowings(function(error, users) {
+    if (error) {
+      done(error);
+      return;
+    }
+    unfollowEveryone(users, done);
+  });
+};
